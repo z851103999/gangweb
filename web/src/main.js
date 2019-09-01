145100d2d2c70fb447467529d3b0972f4c8cdc9b
zhangzheng <zhangzheng@zhangzhengdeMacBook-Pro.local>
2019-09-01 20:37:07 +0800

    静态代码完成一部分

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
 Vue.config.productionTip = false
 
-import './style.scss'
+import './assets/scss/style.scss'
+import './assets/scss/_variables.scss'
 
 new Vue({
   router,
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,10 +1,83 @@
 <template>
   <div>
+    <!-- swiper -->
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img class="w-100" src="../assets/img/0315544165.jpg" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/img/0315544165.jpg" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/img/0315544165.jpg" />
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-1" slot="pagination"></div>
+    </swiper>
+    <!-- end-swiper -->
+    <!-- icon start -->
+    <div class="nav-icon bg-white mt-2 text-center pt-3 text-grey">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="n in 10" :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+       <i class="sprite sprite-arrow mr-1"></i>
+       <span>收起</span>
+      </div>
+    </div>
+    <!-- icon end -->
+
+    <!-- news start  -->
+    <div>
+     
+    </div>
+     <i class="iconfont text-primary">&#xe62c;</i> 
     
+    
+
+
+     <!-- news end -->
+
   </div>
 </template>
+
 <script>
 export default {
-  
-}
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".pagination-home"
+        }
+      }
+    };
+  }
+};
 </script>
+
+<style lang="scss" scope>
+@import "../assets/scss/variables";
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background: map-get($colors, "white");
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, "info");
+    }
+  }
+}
+
+.nav-icon {
+  border-top: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none;
+    }
+  }
+}
+</style>
